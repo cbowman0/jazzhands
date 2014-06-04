533e35387f0c6800ef61dff5751f5540fa97163e
Todd Kover <kovert@omniscient.com>
2014-06-04 17:52:56 -0400

    enforce new account collection trigger rules

--- database/create_pgsql_schema.sql
+++ database/create_pgsql_schema.sql
@@ -45,6 +45,7 @@ SELECT schema_support.build_audit_tables('audit', 'jazzhands');
 \i ddl/schema/pgsql/create_auto_account_coll_triggers.sql
 \i ddl/schema/pgsql/create_device_power_triggers.sql
 \i ddl/schema/pgsql/create_device_coll_hier_triggers.sql
+\i ddl/schema/pgsql/create_account_coll_hier_triggers.sql
 
 -- This could be done for backwards compatibility but is not.
 -- \i compat/pgsql/create_location_compatibility_view.sql
