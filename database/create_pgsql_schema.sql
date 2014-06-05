2e57227073d2251e653287d75b4ad5cb4e5016c8
Todd Kover <kovert@omniscient.com>
2014-06-05 14:45:16 -0400

    add service environment trigger validation and tests

--- database/create_pgsql_schema.sql
+++ database/create_pgsql_schema.sql
@@ -48,6 +48,7 @@ SELECT schema_support.build_audit_tables('audit', 'jazzhands');
 \i ddl/schema/pgsql/create_account_coll_hier_triggers.sql
 \i ddl/schema/pgsql/create_netblock_coll_hier_triggers.sql
 \i ddl/schema/pgsql/create_token_coll_hier_triggers.sql
+\i ddl/schema/pgsql/create_svcenv_coll_hier_triggers.sql
 
 -- This could be done for backwards compatibility but is not.
 -- \i compat/pgsql/create_location_compatibility_view.sql
